d7896b3b7f3411347fe2136fca8eed645ca20dab
bear1278 <grigorijanusik@gmail.com>
2026-04-10 23:05:32 +0300

    add function to set master password

--- passwordManager.go
+++ passwordManager.go
@@ -1,5 +1,7 @@
 package main
 
+import "errors"
+
 type PasswordManager struct {
 	passwords     map[string]Password `json:"passwords"`
 	masterKey     []byte              `json:"-"`
@@ -9,4 +11,19 @@ type PasswordManager struct {
 
 func NewPasswordManager(filePath string) *PasswordManager {
 	return &PasswordManager{filePath: filePath, passwords: make(map[string]Password), isInitialized: false}
+}
+
+/* Важно: Использование пароля напрямую как ключа AES — упрощение для учебных целей.
+В production используют KDF (Key Derivation Function),
+например scrypt или argon2, для безопасного преобразования пароля в ключ.*/
+
+func (pm *PasswordManager) SetMasterPassword(masterPassword string) error {
+	if len(masterPassword) < 8 {
+		return errors.New("master password must be at least 8 characters")
+	}
+	buffer := make([]byte, 32)
+	copy(buffer, []byte(masterPassword))
+	pm.masterKey = buffer
+	pm.isInitialized = true
+	return nil
 }
