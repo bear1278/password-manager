fadd9f00efa89628820216badafe0faf852b5a33
bear1278 <grigorijanusik@gmail.com>
2026-04-10 22:41:36 +0300

    rename fields

--- password.go
+++ password.go
@@ -6,10 +6,10 @@ type Password struct {
 	Name         string    `json:"name"`
 	Value        string    `json:"value"`
 	Category     string    `json:"category"`
-	CreatedAt    time.Time `json:"created"`
-	LastModified time.Time `json:"lastModified"`
+	CreatedAt    time.Time `json:"created_at"`
+	LastModified time.Time `json:"last_modified"`
 }
 
-func NewPassword(value, name, category string) Password {
+func NewPassword(name, value, category string) Password {
 	return Password{Value: value, Name: name, Category: category, CreatedAt: time.Now(), LastModified: time.Now()}
 }
